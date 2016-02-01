8cd7d796acc039634f954d9080c5fd609dbe859b
serge85 <djino85@gmail.com>
2016-02-01 00:31:15 -0200

    adicionando Testes automatizados Cassandra

--- src/test/java/br/com/core/AppTest.java
+++ src/test/java/br/com/core/AppTest.java
@@ -1,38 +1,36 @@
 package br.com.core;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
+
+import org.apache.thrift.transport.TTransportException;
+import org.cassandraunit.CassandraCQLUnit;
+import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.datastax.driver.core.ResultSet;
+import org.apache.cassandra.exceptions.ConfigurationException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	@Rule
+	public CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(
+			new ClassPathCQLDataSet("script.cql", "TestKeyspace"), "/cassandra.yaml", "localhost", 9042);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Before
+	public void setup() throws ConfigurationException, TTransportException, IOException {
+		EmbeddedCassandraServerHelper.startEmbeddedCassandra("/cassandra.yaml");
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void should_have_started_and_execute_cql_script() throws Exception {
+		ResultSet result = cassandraCQLUnit.session.execute("select * from student WHERE id='key1'");
+		assertThat(result.iterator().next().getString("name"), is("Shukla"));
+	}
 }
